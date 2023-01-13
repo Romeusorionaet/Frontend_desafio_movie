a0f01a242ab86bdde3a66f08df3328fe361272fa
romeusorionaet <romeusoares14569@gmail.com>
2023-01-13 11:24:08 -0300

    addInterceptorsApi

--- src/services/api.js
+++ src/services/api.js
@@ -3,3 +3,11 @@ import axios from "axios";
 export const api = axios.create({
     baseURL: 'https://backend-desafio-movie.onrender.com'
 });
+
+api.interceptors.request.use((config) => {
+    const token = window.localStorage.getItem('@rocketmovie:token')
+  
+    if(token) config.headers['Authorization'] = `bearer ${token}`
+    
+     return config
+  })
